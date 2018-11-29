d2e9fc7984aa7d70b07fd63cbfabbf8b774510b9
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-11-29 06:42:02 +0530

    - Querying Data - Building Test Cases

--- product-service/tests/Feature/ExampleTest.php
+++ product-service/tests/Feature/ExampleTest.php
@@ -4,9 +4,11 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+  use DatabaseTransactions;
     /**
      * A basic test example.
      *
@@ -35,5 +37,29 @@ class ExampleTest extends TestCase
     {
       $this->get(route('api.products.index'))
            ->assert(ResponseOk());
+    }
+
+    public function testProductFactoryList()
+    {
+      $products = factory(\App\Product::class,3)->create();
+
+      $this->get(route('api.products.index'))
+           ->assertResponseOk();
+
+      array_map(function($product){
+        $this -> seeJson($product->jsonSerialize());
+      }, $products->all());
+    }
+
+    public function testProuctDescriptions(){
+        $products = factory(\App\Product::class,3)->create();
+        $products->descriptions()->saveMany(factory(\App\Description::class,3)->make());
+
+        $this->get(route('api.products.descriptions.index',['products'=>id]))
+             ->assertResponseOk();
+
+             array_map(function($descriptions){
+               $this -> seeJson($descriptions->jsonSerialize());
+             }, $products->descriptions->all());
     }
 }
